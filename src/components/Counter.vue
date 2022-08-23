b87255a326a7c2792e376f9857ee6b345ccc6d9b
duxiaolei <duxiaolei@mabangerp.com>
2022-08-23 20:08:20 +0800

    add store

---
+++ src/components/Counter.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { countStore } from '@/store/count';
+</script>
+<template>
+  <el-button type="primary">
+    <small>from Counter.vue</small>
+    {{ countStore.count }}
+  </el-button>
+</template>
--- src/views/sys/StoreManager.vue
+++ src/views/sys/StoreManager.vue
@@ -1,8 +1,20 @@
 <template>
   <div>store manager</div>
   <div class="grid">
-    <section>1</section>
+    <section>
+      <el-button @click="countStore.count++">
+        Count ++ {{ countStore.count }}
+      </el-button>
+      <Counter></Counter>
+    </section>
     <section>2</section>
     <section>3</section>
   </div>
 </template>
+
+<script setup lang="ts">
+import Counter from '@/components/Counter.vue';
+import { countStore } from '@/store/count';
+
+countStore;
+</script>
